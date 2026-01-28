16c90ff85f1e13d7e08c769f3e83974f13c518c3
jy4education_git <jy4education@gmail.com>
2026-01-28 12:13:51 +0100

    +

---
+++ com/cc/java/SuperCat.java
@@ -0,0 +1,28 @@
+package com.cc.java;
+
+public class SuperCat
+ {
+
+  private String firstName;
+  private String furColor;
+  private int age;
+
+  public SuperCat(String firstName, String furColor, int age) {
+    this.firstName = firstName;
+    this.furColor = furColor;
+    this.age = age;
+  }
+
+  public String getStringAttributes(String op) {
+    switch (op) {
+      case "#name":
+        return firstName;
+      case "#color":
+        return furColor;
+      default:
+        return "ERROR!";
+    }
+  }
+
+}
+  
