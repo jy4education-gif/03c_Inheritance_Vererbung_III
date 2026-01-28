4ac1a4d8529aaa3e305cb982e82a32f522b1e7d7
jy4education_git <jy4education@gmail.com>
2026-01-28 12:24:46 +0100

    +

--- com/cc/java/Tomcat.java
+++ com/cc/java/Tomcat.java
@@ -1,26 +1,9 @@
 package com.cc.java;
 
-public class Tomcat {
-
-  private String firstName;
-  private String furColor;
-  private int age;
+public class Tomcat extends SuperCat {
 
   public Tomcat(String firstName, String furColor, int age) {
-    this.firstName = firstName;
-    this.furColor = furColor;
-    this.age = age;
-  }
-
-  public String getStringAttributes(String op) {
-    switch (op) {
-      case "#name":
-        return firstName;
-      case "#color":
-        return furColor;
-      default:
-        return "ERROR!";
-    }
+    super(firstName, furColor, age);
   }
 
   public String getAge() {
